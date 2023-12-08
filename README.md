211d88cc6bc36a23043cdf55b18d8dc7fdd42412
jamesdev28 <109198384+jamesdev28@users.noreply.github.com>
2023-12-07 18:51:49 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+### Run server: 
+In the project root directory, run
+- `npm install`
+- `npm start`
+
+### Run React client: 
+- `cd frontend`
+- `npm install`
+- `npm start`
+Then the website goes live at localhost:3000
+
+### Run React production build
+To run React production build, at the project root directory, run
+- `npm run build`
+This will build the React directory and create a frontend/build directory. Then the website goes live at localhost:5000 because now it's served via the NodeJS server
---
+++ src/components/navbar.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+export default class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <Link to="/" className="navbar-brand">Your Meal</Link>
+                <div className="collpase navbar-collapse">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="navbar-item">
+                            <Link to="/" className="nav-link">List API</Link>
+                        </li>
+                        <li className="navbar-item">
+                            <Link to="/create" className="nav-link">Add an Item</Link>
+                        </li>
+                        <li className='navbar-item'>
+                            <Link to="/mongo" className='nav-link'>Show Datas</Link>
+                        </li>
+
+                    </ul>
+                    
+                </div>
+            </nav>
+        );
+    }
+}
